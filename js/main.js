c547a98dde9f8e3aa6ca33e69e1b6f3969e17aa8
ina <chapa_071@mail.ru>
2024-03-21 22:08:40 +0700

    сделала 2.7

--- js/main.js
+++ js/main.js
@@ -35,3 +35,24 @@ document.getElementById('seasons').addEventListener('click', () => {
     const input = prompt('Введите номер месяца')
     getSeason(input)
 })
+
+// task 11
+
+function fruitGame() {
+    let fruits = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    fruits = fruits.sort(() => Math.random() - 0.5);
+    alert(fruits);
+    let firstGuess = prompt("Чему равнялся первый элемент массива?").toLowerCase();
+    let lastGuess = prompt("Чему равнялся последний элемент массива?").toLowerCase();
+    if (firstGuess === fruits[0].toLowerCase() && lastGuess === fruits[fruits.length - 1].toLowerCase()) {
+        alert("Поздравляем! Вы угадали оба слова.")
+    } else if (firstGuess === fruits[0].toLowerCase() || lastGuess === fruits[fruits.length - 1].toLowerCase()) {
+        alert("Вы были близки к победе!");
+    } else {
+        alert("Вы не угадали ни одного слова.");
+    }
+}
+
+document.getElementById('words').addEventListener('click', () => {
+    fruitGame()
+})
